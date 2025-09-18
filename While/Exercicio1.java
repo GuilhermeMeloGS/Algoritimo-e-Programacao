02eeb83395b151ee75c19800c6949cf404b69fac
Guilherme Melo <guilhermemelogsantos@gmail.com>
2025-09-18 08:57:27 -0300

    Exercicio1.java

---
+++ While/Exercicio1.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main (String []args){
+        System.out.println("Contador de 100 a 200");
+        int numero = 100;
+        
+        while(numero <= 200){
+            System.out.println(numero);
+            numero++;
+        }
+    }
+}
