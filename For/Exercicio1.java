1b83440b6bcd10d361fbbce491fd61af26db02c5
Guilherme Melo <guilhermemelogsantos@gmail.com>
2025-09-25 21:19:53 -0300

    Create Exercicio1.java

---
+++ For/Exercicio1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Vamos imprimir uma sequência");
+        for(int num =100; num >= 0; num--){
+            System.out.print(num);
+        }
+    }
+}
