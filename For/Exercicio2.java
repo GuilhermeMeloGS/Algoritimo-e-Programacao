986f6099820dd2bf67ef6bc080c5cb31acec5c36
Guilherme Melo <guilhermemelogsantos@gmail.com>
2025-09-25 21:20:10 -0300

    Create Exercicio2.java

---
+++ For/Exercicio2.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        for(int num =15; num<=100; num++){
+            int quadrado = num*num;
+            System.out.printf("O quadrado do número %d é %d\n",num,quadrado);
+        }
+    }
+}
